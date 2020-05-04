e727d2ed822ce4a5d0373add44b155032388184c
Arup <arupctg@gmail.com>
2020-05-04 08:57:39 -0800

    htaccess file

---
+++ app/controllers/Home.php
@@ -0,0 +1,13 @@
+ <?php
+ 
+ class Home extends Controller{
+     public function index(){
+         echo "home";
+     }
+ }
+ 
+ 
+ 
+ 
+ 
+ ?>
--- app/core/App.php
+++ app/core/App.php
@@ -1,9 +1,21 @@
 <?php
 
 class App{
-    
+
+     protected $controller = 'home';
+
+     protected $method = 'index';
+
+     protected $params =[];
+     
      public function __construct(){
-        echo "oh";
+        $url = $this->parseUrl();
+     }
+
+     public function parseUrl(){
+         if(isset($_GET['url'])){
+            return $url = explode('/',filter_var(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL));
+         }
      }
 }
 
